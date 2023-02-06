ac96a880b226e252fc7a17aeeab2f8a5540363ae
Queren Hapuque <queren.fernandes@yahoo.com>
2023-02-06 12:47:50 -0300

    adicionando os outros carros

--- carro.js
+++ carro.js
@@ -1,13 +1,30 @@
-//CARRO
+// CÓDIGO DO CARRO
+
+//CARRO 1
 let xCarro = 600;
 let yCarro = 40;
+let velocidadeCarro1 = 2;
+
+//CARRO 2
+let xCarro2 = 600;
+let yCarro2 = 96;
+let velocidadeCarro2 = 3;
+
+//CARRO 3
+let xCarro3 = 600;
+let yCarro3 = 150;
+let velocidadeCarro3 = 2.5;
 
 function mostraCarro() {
     image(imagemCarro, xCarro, yCarro, 50, 40);
+    image(imagemCarro2, xCarro2, yCarro2, 50, 40);
+    image(imagemCarro3, xCarro3, yCarro3, 50, 40);
   }
   
   function movimentaCarro() {
-    xCarro -= 2;
+    xCarro -= velocidadeCarro1;
+    xCarro2 -= velocidadeCarro2;
+    xCarro3 -= velocidadeCarro3;
   }
   
   
--- imagens.js
+++ imagens.js
@@ -1,9 +1,13 @@
 let imagemDaEstrada;
 let imagemDoAtor;
 let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
 
 function preload(){
     imagemDaEstrada = loadImage("img/estrada.png");
     imagemDoAtor = loadImage("img/ator-1.png");
-    imagemCarro = loadImage("img/carro-1.png");  
+    imagemCarro = loadImage("img/carro-1.png");
+    imagemCarro2 = loadImage("img/carro-2.png"); 
+    imagemCarro3 = loadImage("img/carro-3.png");   
 }
