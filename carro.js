dfc8b4ecee2c76f13b04fd9398b6dd51f381ba83
Queren Hapuque <queren.fernandes@yahoo.com>
2023-02-07 12:08:45 -0300

    otimizando o codigo com funcções

--- carro.js
+++ carro.js
@@ -1,6 +1,5 @@
 // CÓDIGO DO CARRO
 
-
 //Criando listas para otimizar o código. Chama o elemento pelo índice.
 let xCarros = [600, 600, 600];
 let yCarros = [40, 96, 150 ];
@@ -12,19 +11,19 @@ function mostraCarro(){
   }
 }
   function movimentaCarro() {
-    xCarros[0] -= velocidadeCarros[0];
-    xCarros[1] -= velocidadeCarros[1];
-    xCarros[2] -= velocidadeCarros[2];
+    for (let i = 0; i < velocidadeCarros.length; i = i + 1){
+      xCarros[i] -= velocidadeCarros[i];
+    }
   }
   
   function voltaPosicaoInicialDoCarro() {
-    if (xCarros[0] < -50 ){
-      xCarros[0] = 600;
-    }
-    if (xCarros[1] < -50 ){
-      xCarros[1] = 600;
-    }
-    if (xCarros[2] < -50 ){
-      xCarros[2] = 600;
-    }
+    for(i = 0; i < imagemCarros.length; i = i +1){
+      if (passouTodaATela(xCarros[i])){
+        xCarros[i] = 600;
+      }
+    } 
+  }
+
+  function passouTodaATela(xCarro) {
+   return xCarro < -50;
   }
